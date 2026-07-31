3f343a399e3a6c93151d5ef39b60de811666cd65
Kenny Glowner <SisyphusAeolides@pm.me>
2026-07-31 00:26:40 -0500

    Lock the installer to the refreshed package snapshot

--- src/installer.rs
+++ src/installer.rs
@@ -22,7 +22,7 @@ pub const BOOT_BUNDLE_SCHEMA: u32 = 1;
 const MAX_BOOT_ARTIFACT_BYTES: u64 = 32 * 1024 * 1024;
 const MAX_RECOVERY_TRANSACTIONS: usize = 128;
 const HARDWARE_RECIPES_URL: &str = "https://github.com/SisyphusAeolides/Arach-Packages.git";
-const HARDWARE_RECIPES_REVISION: &str = "cbf6ee068aa7e7b1143e4dfaf79132a9a211d996";
+const HARDWARE_RECIPES_REVISION: &str = "e62155d286d8b2aac79e588bce1a7fa980148d8c";
 static TEMPORARY_SERIAL: AtomicU64 = AtomicU64::new(1);
 
 const BOOT_MANIFEST_NAME: &str = "manifest.json";
